4e0d90c12601a4254ef8d93fb5f8863f60591fd3
Josh Rochon <joshdrochon@gmail.com>
2018-02-22 12:14:54 -0800

    create City model

---
+++ WorldData.Tests/ModelTests/CityTests.cs
@@ -0,0 +1,5 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using WorldDataProject.Models;
+using WorldDataProject;
+using System;
--- WorldData/Models/Country.cs
+++ WorldData/Models/Country.cs
@@ -7,11 +7,11 @@ namespace WorldDataProject.Models
 {
   public class Country
   {
-    private static List<Country> allCountries; //getAll method will return this list
-    private string _code; //index 0
-    private string _name; //index 1
-    private string _continent; //index 2
-    private int _population; //index 6
+    private static List<Country> _allCountries; //getAll method will return this list
+    private string _code; //[0]
+    private string _name; //[1]
+    private string _continent; //[2]
+    private int _population; //[6]
 
     public Country(string Code, string Name, string Continent, int Population)
     {
@@ -21,85 +21,88 @@ namespace WorldDataProject.Models
       _population = Population;
     }
 
-    //code setter/getter
-    public void SetCode(string newCode)
-    {
-      _code = newCode;
-    }
+    //_code getter/setter
     public string GetCode()
     {
       return _code;
     }
-    //name setter/getter
-    public void SetName(string newName)
+    public void SetCode(string newCode)
     {
-      _name = newName;
+      _code = newCode;
     }
+
+    //_name getter/setter
     public string GetName()
     {
       return _name;
     }
-
-    //continent setter/getter
-    public void SetContinent(string newContinent)
+    public void SetName(string newName)
     {
-      _continent = newContinent;
+      _name = newName;
     }
+
+    //_continent getter/setter
     public string GetContinent()
     {
       return _continent;
     }
-
-    //population setter/getter
-    public void SetPopulation(int newPopulation)
+    public void SetContinent(string newContinent)
     {
-      _population = newPopulation;
+      _continent = newContinent;
     }
+
+
+    //_population getter/setter
     public int GetPopulation()
     {
       return _population;
     }
+    public void SetPopulation(int newPopulation)
+    {
+      _population = newPopulation;
+    }
 
-        public static List<Country> GetAll() /*returns a List which contains Country objects*/
+
+    public static List<Country> GetAll() /*returns a List which contains Country objects*/
+    {
+        MySqlConnection conn = DB.Connection(); //MySqlConnection represents database, calling DB.Connection establishes a connection w/ mysql database
+        conn.Open(); //opens the connection
+        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"SELECT * FROM country;";
+        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+        while(rdr.Read())
         {
-            MySqlConnection conn = DB.Connection(); //MySqlConnection represents database, calling DB.Connection establishes a connection w/ mysql database
-            conn.Open(); //opens the connection
-            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"SELECT * FROM country;";
-            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-            while(rdr.Read())
-            {
-              string countryCode = rdr.GetString(0);
-              string countryName = rdr.GetString(1);
-              string countryCont = rdr.GetString(2);
-              int countryPop = rdr.GetInt32(6);
-              Country newCountry = new Country(countryCode, countryName, countryCont, countryPop);
-              allCountries.Add(newCountry);
-            }
-            conn.Close();
-            if (conn != null)
-            {
-                conn.Dispose();
-            }
-            return allCountries;
+          string countryCode = rdr.GetString(0);
+          string countryName = rdr.GetString(1);
+          string countryCont = rdr.GetString(2);
+          int countryPop = rdr.GetInt32(6);
+          Country newCountry = new Country(countryCode, countryName, countryCont, countryPop);
+          _allCountries.Add(newCountry);
         }
-
-        public static void DeleteAll()
+        conn.Close();
+        if (conn != null)
         {
-          MySqlConnection conn = DB.Connection();
-          conn.Open();
-
-          var cmd = conn.CreateCommand() as MySqlCommand;
-          cmd.CommandText = @"DELETE FROM country;";
-
-          cmd.ExecuteNonQuery();
-
-          conn.Close();
-          if (conn != null)
-          {
-              conn.Dispose();
-          }
-          allCountries.Clear();
+            conn.Dispose();
         }
+        return allCountries;
+    }
+
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM country;";
+
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+      if (conn != null)
+      {
+          conn.Dispose();
+      }
+      _allCountries.Clear();
+    }
   }
 }
